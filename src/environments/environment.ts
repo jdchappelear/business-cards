ae64a738a96f8e3f4a8c69e6b6d2adb5744ea44a
John Chappelear <john.chappelear@gmail.com>
2019-11-17 14:58:12 -0500

    Testing .gitignore to make sure it's ignoring

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,8 @@ export const environment = {
   production: false
 };
 
+// Test of "SECRET" entry - this line should not be included in online repo
+
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
