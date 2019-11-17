abeb1b6923916188173d0ea24ad92bf0cc466d53
John Chappelear <john.chappelear@gmail.com>
2019-11-17 16:27:13 -0500

    Added 4 basic components (non-optional)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BusinesscardComponent } from './businesscard/businesscard.component';
+import { BusinesscardsComponent } from './businesscards/businesscards.component';
+import { LoginComponent } from './login/login.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BusinesscardComponent,
+    BusinesscardsComponent,
+    LoginComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
