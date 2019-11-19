34e7f89ef3f78120278201c8cf872ce001940a37
John Chappelear <john.chappelear@gmail.com>
2019-11-19 09:54:15 -0500

    Trying to get Bootstrap working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+//import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BusinesscardComponent } from './businesscard/businesscard.component';
 import { BusinesscardsComponent } from './businesscards/businesscards.component';
-import { LoginComponent } from './login/login.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     BusinesscardComponent,
     BusinesscardsComponent,
-    LoginComponent,
     NotfoundComponent,
     SignupComponent,
     SigninComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
